0b1925102ae42f17f8227aea14a03af3face9ebf
Dmitriy Rubekin <rda1902@gmail.com>
2022-04-14 14:35:58 +0400

    change keccak version

--- lib/web3/eth/abi/utils.rb
+++ lib/web3/eth/abi/utils.rb
@@ -1,7 +1,7 @@
 # -*- encoding : ascii-8bit -*-
 
 require 'digest'
-require 'digest/sha3'
+require 'digest/keccak'
 require 'openssl'
 require 'rlp'
 
@@ -16,11 +16,11 @@ module Web3::Eth::Abi
     # Not the keccak in sha3, although it's underlying lib named SHA3
     #
     def keccak256(x)
-      Digest::SHA3.new(256).digest(x)
+      Digest::Keccak.new(256).digest(x)
     end
 
     def keccak512(x)
-      Digest::SHA3.new(512).digest(x)
+      Digest::Keccak.new(512).digest(x)
     end
 
     def keccak256_rlp(x)
